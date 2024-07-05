46900ce216d42beaec55d64dddb42257ce30f209
Scott Lu <ylu@ten-x.com>
2024-07-05 13:11:37 -0700

    before setup database

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Noto_Sans_HK, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const noto = Noto_Sans_HK({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+  });
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+  });
