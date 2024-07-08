3d38d2ebd6538d3965e725e349f3ff3a76d65303
qingcai <qingcai@gmail.com>
2024-07-07 23:29:50 -0700

    chapter10

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,7 @@
 const styleguide = require('@vercel/style-guide/prettier');
 
 module.exports = {
+  // endOfLine: 'cr'
   ...styleguide,
   plugins: [...styleguide.plugins, 'prettier-plugin-tailwindcss'],
 };
